13f5a41bcec9c399becd4616eb49725de5f5e5ed
Omega <47953064+Panda-Dreamer@users.noreply.github.com>
2022-06-04 14:03:11 +0000

    e

--- js/handler2.js
+++ js/handler2.js
@@ -8,15 +8,17 @@ document.getElementById("fileSelect").addEventListener("change", (e) => {
   formData.append("audio", selectedFile);
   formData.append("idOnly", true);
   console.log("Sending files...");
-  fetch("http://129.151.73.110/api/analyze", {
+  fetch("https://cui-cui.ml/api/analyze", {
     method: "post",
     body: formData,
-  }).then(id=>{
-    console.log('response:',id)
-    if(id.status == true){
-    document.cookie=`cuicuiml_token=${id.uuid}`;
-    location.href = "result.php"
-    }
+  }).then(res=>{
+    res.json().then((data) => {
+          console.log(data)
+          if(data.status == true){
+          document.cookie=`cuicuiml_token=${data.uuid}`;
+          location.href = "result.php"
+          }
+    })
   })
 })
 
