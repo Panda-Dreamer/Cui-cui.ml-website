3d92cd1a682d06b3b832ada64c23348e2154ca63
Omega <47953064+Panda-Dreamer@users.noreply.github.com>
2022-06-04 16:16:31 +0200

    Update handler2.js

--- js/handler2.js
+++ js/handler2.js
@@ -11,12 +11,14 @@ document.getElementById("fileSelect").addEventListener("change", (e) => {
   fetch("https://cui-cui.ml/api/analyze", {
     method: "post",
     body: formData,
-  }).then(id=>{
-    console.log('response:',id)
-    if(id.status == true){
-    document.cookie=`cuicuiml_token=${id.uuid}`;
-    location.href = "result.php"
-    }
+  }).then(res=>{
+    res.json().then((data) => {
+          console.log(data)
+          if(data.status == true){
+          document.cookie=`cuicuiml_token=${data.uuid}`;
+          location.href = "result.php"
+          }
+    })
   })
 })
 
