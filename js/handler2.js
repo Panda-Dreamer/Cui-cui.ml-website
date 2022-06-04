c4a258cda1fcf01c5777721b68657d79e7cd7a36
Panda-Dreamer <castoretpapillon@gmail.com>
2022-06-04 21:29:07 +0200

    changes

--- js/handler2.js
+++ js/handler2.js
@@ -1,8 +1,9 @@
 document.getElementById("fileSelect").addEventListener("change", (e) => {  
   e.preventDefault();
+  reset_button()
   console.log("Cliked file select");
   const selectedFile = document.getElementById("fileSelect").files[0];
-
+  //21:26
   const formData = new FormData();
   formData.append("complete", true);
   formData.append("audio", selectedFile);
@@ -20,19 +21,56 @@ document.getElementById("fileSelect").addEventListener("change", (e) => {
           }
     })
   })
+  .catch(err=>{
+    console.log("An error occured while uploading")
+    upload_error()
+  })
 })
 
 function analyse() {
   setTimeout(analyse_suite, 2000);
 }
 
+function error_timeout(){
+  setTimeout(upload_error, 2000);
+}
+
+function reload() {
+  location.href = 'index.php'
+}
 function analyse_suite() {
+  if(document.querySelector(".btn_upload") != null){
   document.querySelector(".txt_upload").innerText ="Analyse en cours...";
   document.querySelector(".btn_upload").className = "btn_already_uploaded";
   document.querySelector(".img_btn_upload").style.display="none";
   document.querySelector(".img_analyse").style.display="block";
+  }
 }
 
+function upload_error(){
+  if(document.querySelector(".btn_upload") != null){
+  document.querySelector(".txt_upload").innerText ="Erreur de l'upload";
+  document.querySelector(".btn_upload").className = "btn_error";
+  document.querySelector(".img_btn_upload").style.display="none";
+  document.querySelector(".img_analyse").style.display="block";
+  }
+  if(document.querySelector(".btn_already_uploaded") != null){
+  document.querySelector(".txt_upload").innerText ="Erreur de l'upload";
+  document.querySelector(".img_btn_upload").style.display="none";
+  document.querySelector(".btn_already_uploaded").className = "btn_error";
+  document.querySelector(".img_analyse").style.display="block";
+  }
+}
+
+function reset_button(){
+  if(document.querySelector(".btn_error") != null){
+  document.querySelector(".txt_upload").innerText ="Analyse en cours...";
+  document.querySelector(".btn_error").className = "btn_upload";
+  document.querySelector(".img_btn_upload").style.display="none";
+  document.querySelector(".img_analyse").style.display="block";
+  }
+}
 function erreur(){
   alert("L'enregistrement sonore n'est pas encore disponible")
 }
+
